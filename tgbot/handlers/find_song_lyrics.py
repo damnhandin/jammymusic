d77b71acebc4342dd8560b74c884a8db0c63a692
Raindeero <raindeero@mail.ru>
2023-05-27 12:49:13 +0200

    similar and lyrics done

--- tgbot/handlers/find_song_lyrics.py
+++ tgbot/handlers/find_song_lyrics.py
@@ -1,10 +1,39 @@
 from aiogram import types, Dispatcher
 from aiogram.dispatcher.filters import Text
+from aiogram.types import ContentType
+
+from tgbot.misc.states import JammyMusicStates
+from tgbot.config import Config
+import lyricsgenius
 
 
-async def find_song_lyrics(message: types.Message):
-    pass
+async def find_lyrics(message: types.Message):
+    await JammyMusicStates.find_lyrics.set()
+    await message.answer("Отправь мне название трека и я верну тебе текст песни, если найду")
 
+
+async def get_lyrics(message: types.Message, config: Config, state):
+    await state.reset_state()
+    try:
+        lyrics_genius = lyricsgenius.Genius(config.tg_bot.genius_token)
+        result = lyrics_genius.search_song(message.text)
+        m = result.lyrics
+        if len(m) > 4095:
+            for x in range(0, len(m), 4095):
+                await message.answer(m[x:x + 4095])
+        else:
+            await message.answer(result.lyrics)
+    except Exception:
+        await message.answer("К сожалению, нам не удалось найти текст данной песни")
+
+
+async def get_unknown_content_to_find_lyrics(message: types.Message):
+    await message.answer("Похоже, что вы хотели найти текст песни, но мы получили от вас неизвестный формат файла, "
+                         "пожалуйста, убедитесь в том, что вы действительно отправили только текст.")
 
 def register_find_lyrics(dp: Dispatcher):
-    dp.register_message_handler(find_song_lyrics, Text("📄 Найти текст песни"))
+    dp.register_message_handler(find_lyrics, Text("📄 Найти текст песни"))
+    dp.register_message_handler(get_lyrics, content_types=ContentType.TEXT,
+                                state=JammyMusicStates.find_lyrics)
+    dp.register_message_handler(get_unknown_content_to_find_lyrics, content_types=ContentType.ANY,
+                                state=JammyMusicStates.find_lyrics)
--- tgbot/handlers/similar_songs_search.py
+++ tgbot/handlers/similar_songs_search.py
@@ -1,10 +1,53 @@
 from aiogram import types, Dispatcher
 from aiogram.dispatcher.filters import Text
+from aiogram.types import ContentType
+
+from tgbot.misc.states import JammyMusicStates
+
+from shazamio import Shazam
 
 
 async def similar_songs_search(message: types.Message):
-    pass
+    await JammyMusicStates.shazam_recomendation.set()
+    await message.answer("Отправь мне имя исполнителя и название трека (например Моргенштерн aristocrat), а я попробую "
+                     "тебе посоветовать похожее")
+
+
+async def shazam_recomendation_search(message: types.Message, state):
+    await state.reset_state()
+    shazam = Shazam()
+    try:
+        tracks = await shazam.search_track(query=message.text, limit=5)
+        print(tracks)
+        list = {}
+        if tracks:
+            count_of_finded_songs = len(tracks['tracks']['hits'])
+            for i in range(count_of_finded_songs):
+                info = tracks['tracks']['hits'][i]['heading']
+                id = int(tracks['tracks']['hits'][i]['actions'][0]['id'])
+            try:
+                related = await shazam.related_tracks(track_id=id, limit=10)
+                tracks = []
+                for i in range(10):
+                    tracks.append(related['tracks'][i]['subtitle'] + " - " + related['tracks'][i]['title'])
+                s = '\n'.join(tracks)
+                await message.answer("Похожие треки:" + "\n" + s + "\n" + "Больше музыки на @jammy_music_bot")
+            except Exception:
+                await message.answer("Я не знаю, что тебе посоветовать")
+        else:
+            await message.answer("Я не знаю, что тебе посоветовать")
+    except Exception:
+        await message.answer("Я не знаю, что тебе посоветовать")
+
+
+async def get_unknown_content_to_find_similar(message: types.Message):
+    await message.answer("Мы получили от вас неизвестный формат файла, "
+                         "пожалуйста, убедитесь в том, что вы действительно отправили только текст.")
 
 
 def register_similar_songs_search(dp: Dispatcher):
     dp.register_message_handler(similar_songs_search, Text("🎼 Найти похожие треки"))
+    dp.register_message_handler(similar_songs_search, content_types=ContentType.TEXT,
+                                state=JammyMusicStates.shazam_recomendation)
+    dp.register_message_handler(get_unknown_content_to_find_similar, content_types=ContentType.ANY,
+                                state=JammyMusicStates.shazam_recomendation)
