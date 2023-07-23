66f630af06fccd06b79d69beae982ee91bb30e58
damnhandin <lushnikovff@gmail.com>
2023-07-23 14:45:12 +0200

    BIGFIX for raindeero commit, change sending architecture

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from tgbot.config import load_config
 from tgbot.filters.admin import AdminFilter
 from tgbot.filters.check_terms_filter import CheckUserFilter
 from tgbot.handlers.add_own_song import register_add_own_music
-from tgbot.handlers.admin import register_admin
+from tgbot.handlers.admin import register_admin_handlers
 from tgbot.handlers.check_user_handlers import register_check_user_handlers
 from tgbot.handlers.conditional_terms import register_conditional_terms_handlers
 from tgbot.handlers.find_song_lyrics import register_find_lyrics
@@ -21,7 +21,6 @@ from tgbot.handlers.shazam import register_shazam
 from tgbot.handlers.similar_songs_search import register_similar_songs_search
 from tgbot.handlers.text_button_registration import text_button_registration
 from tgbot.handlers.thanks_to_devs import register_thanks_to_devs_handlers
-from tgbot.handlers.update_message_func import register_update
 from tgbot.handlers.user import register_user
 from tgbot.middlewares.album import AlbumMiddleware
 from tgbot.middlewares.environment import EnvironmentMiddleware
@@ -57,8 +56,8 @@ async def delete_all_not_valid_subs(db):
     sql = """
     SELECT * FROM active_subscriptions WHERE subscription_date_end < $1;
     """
-    non_valid_subs = await db.execute(sql, fetch=True)
     current_date = datetime.now()
+    non_valid_subs = await db.execute(sql, current_date, fetch=True)
     for user in non_valid_subs:
         await db.activate_user_sub(user["telegram_id"], current_date)
 
@@ -88,26 +87,23 @@ def register_all_filters(dp):
 
 
 def register_all_handlers(dp, db):
+    # admin handlers
+    register_admin_handlers(dp)
     # Здесь хендлеры которые не сбрасывают состояние
     register_conditional_terms_handlers(dp)
     register_check_user_handlers(dp, db)
     # Самое главное чтобы в верхних хендлерах не сбрасывалось состояние
     register_payment(dp)
     text_button_registration(dp)
-    register_admin(dp)
-    register_update(dp)
     register_user(dp)
     register_similar_songs_search(dp)
     register_shazam(dp)
     register_find_lyrics(dp)
     register_find_song_by_words(dp)
     register_add_own_music(dp)
-    # register_find_song(dp)
-
     register_search_music(dp)
-
-    # register_echo(dp)
     register_thanks_to_devs_handlers(dp)
+
 
 
 async def main():
