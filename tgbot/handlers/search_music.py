f01de3077fd2af51b745ff6edf9904f984d83d33
Raindeero <raindeero@mail.ru>
2023-09-30 14:19:46 +0200

    1) меня вылетает ошибка когда пытаюсь достать токен 2) надо как то запомнить ya_tracks, в каллбеках кнопок цифры от 0 до 3, при нажатии кнопки надо отдельно потом прописывать скачивание по индексу

--- tgbot/handlers/search_music.py
+++ tgbot/handlers/search_music.py
@@ -1,19 +1,21 @@
 from aiogram import Dispatcher, types
-from aiogram.types import ContentType, InputFile
+from aiogram.types import ContentType, InputFile, InlineKeyboardMarkup
 from aiogram.utils.exceptions import MessageIsTooLong
 import aiogram.utils.markdown as fmt
 from pytube import YouTube
 from pytube.exceptions import AgeRestrictedError
 from youtubesearchpython import VideosSearch, Video, ResultMode
+from yandex_music import ClientAsync
+from tgbot.config import Config
 
 from tgbot.keyboards.inline import music_msg_keyboard
 from tgbot.misc.exceptions import FileIsTooLarge
 
 from tgbot.misc.misc_funcs import convert_search_results_to_reply_markup, filter_songs_without_correct_duration, \
-    get_audio_file_from_yt_video, run_cpu_bound, run_blocking_io
+    get_audio_file_from_yt_video, run_cpu_bound, run_blocking_io, convert_music_api_search_res_to_reply_markup
 
 
-async def search_music_func(mes: types.Message):
+async def search_music_func(mes: types.Message, config: Config):
     msg_text = fmt.text(mes.text)
     try:
         video = Video.get(msg_text, mode=ResultMode.dict, get_upload_date=True)
@@ -43,13 +45,34 @@ async def search_music_func(mes: types.Message):
                                reply_markup=music_msg_keyboard, caption='Больше музыки на @jammy_music_bot')
         return
     except Exception:
-        video_searcher = await run_blocking_io(VideosSearch, msg_text, 8, 'ru-RU', 'RU')
+        video_searcher = await run_blocking_io(VideosSearch, msg_text, 4, 'ru-RU', 'RU')
         search_results = await run_cpu_bound(filter_songs_without_correct_duration, video_searcher)
         if not search_results:
             await mes.answer("Никаких совпадений по запросу.")
             return
         reply_markup = await run_cpu_bound(convert_search_results_to_reply_markup, search_results)
+        # answer = f'{fmt.hbold("Результаты по запросу")}: {fmt.hcode(msg_text)}'
+        # try:
+        #     await mes.answer(answer, reply_markup=reply_markup, disable_web_page_preview=False)
+        # except MessageIsTooLong:
+        #     await mes.answer(f'{fmt.hbold("Результаты по вашему запросу:")}', reply_markup=reply_markup)
+        ####################################################################
+        #client = await ClientAsync(f'{config.tg_bot.y_token}').init() dont work
+        client = await ClientAsync('y0_AgAAAABw7WXsAAG8XgAAAADtKMqC_mWtBnYQSyOr5luJ142zrDOcm6o').init()
 
+        ya_search = await client.search(mes.text)
+        ya_tracks = ya_search.tracks.results[:4]
+        if ya_tracks:
+            ya_search_res_reply_markup = await run_cpu_bound(convert_music_api_search_res_to_reply_markup, ya_tracks)
+            search_results_reply_markup = await run_cpu_bound(convert_search_results_to_reply_markup,
+                                                              search_results[:4])
+            ya_buttons = ya_search_res_reply_markup.inline_keyboard
+            search_buttons = search_results_reply_markup.inline_keyboard
+            combined_buttons = ya_buttons + search_buttons
+
+            reply_markup = InlineKeyboardMarkup(inline_keyboard=combined_buttons)
+        else:
+            reply_markup = await run_cpu_bound(convert_search_results_to_reply_markup, search_results)
         answer = f'{fmt.hbold("Результаты по запросу")}: {fmt.hcode(msg_text)}'
         try:
             await mes.answer(answer, reply_markup=reply_markup, disable_web_page_preview=False)
